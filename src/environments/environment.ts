f830c27e784c63f2a2603d4ee797f95c752524b1
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2024-07-31 23:40:57 +0330

    firebase modules

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyBQECipTT6JJYj7f9-9_5lJDlvWTcWEGfI",
+        authDomain: "fitness-tracker-mmf.firebaseapp.com",
+        projectId: "fitness-tracker-mmf",
+        storageBucket: "fitness-tracker-mmf.appspot.com",
+        messagingSenderId: "662164851298",
+        appId: "1:662164851298:web:3e935a1a0daf6f6a999f3b"
+      }
+};
